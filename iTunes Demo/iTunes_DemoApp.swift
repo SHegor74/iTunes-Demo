14d7acb23168a44bad3e98a5d9fe9b4c29cdb859
SHegor <emnaberezhnov@gmail.com>
2024-09-18 14:04:37 +0300

    Initial Commit

---
+++ iTunes Demo/iTunes_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunes_DemoApp.swift
+//  iTunes Demo
+//
+//  Created by Egor Naberezhnov on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct iTunes_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
